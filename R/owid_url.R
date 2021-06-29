77b83b741e8698578d4085796613de5bffcb5312
piersyork <85755776+piersyork@users.noreply.github.com>
2021-06-29 10:35:49 +0100

    update owid_url

--- R/owid_url.R
+++ R/owid_url.R
@@ -1,12 +1,12 @@
 #' Get the GitHub url for the data
 #'
-#' @param data A tibble returned from `owid`
+#' @param data A tibble returned from `owid()`
 #'
 #' @return The GitHub url as a character string.
 #' @export
 #'
 
-owid_get_url <- function(data) {
+owid_url <- function(data) {
   if (!class(data)[1] == "owid") {
     stop("Data must be of class owid")
   }
