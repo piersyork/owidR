433cf8aa01e6a3821c3e2681011cec374455f099
piersyork <85755776+piersyork@users.noreply.github.com>
2021-07-17 14:53:52 +0100

    update map

--- R/owid_map.R
+++ R/owid_map.R
@@ -6,8 +6,9 @@
 #' @param data A tibble returned by `owid()`. This tibble must have country names as the Entity, not all data returned by `owid()` will be like this.
 #' @param col The column number to be treated as the value.
 #' @param palette The `RColorBrewer` palette to be used.
+#' @param mode If `"plot"`, the output will be a ggplot2 map. If `"view"`, the output will be a leaflet interactive map.
 #'
-#' @return Either a ggplot
+#' @return Either a ggplot2
 #' @export
 #'
 #' @examples
@@ -33,10 +34,10 @@ owid_map <- function(data, col = 3, palette = "Reds", mode = "plot") {
                               "Russian Federation" = "Russia",
                               "Lao PDR" = "Laos",
                               "Dem. Rep. Korea" = "North Korea",
-                              "Republic of Korea" = "South Korea"))
-
-  world$NAME_LONG %>% grep("Korea", ., value = TRUE)
-  data$Entity %>% unique() %>% grep("Korea", ., value = TRUE)
+                              "Republic of Korea" = "South Korea",
+                              "Somaliland" = "Somalia")) %>%
+    group_by(owid_name) %>%
+    summarise()
 
   test <- ggplot2::ggplot(world) +
     ggplot2::geom_sf()
@@ -61,7 +62,7 @@ owid_map <- function(data, col = 3, palette = "Reds", mode = "plot") {
   if (mode == "plot") {
     map_data %>%
       ggplot2::ggplot(ggplot2::aes(fill = value, id = owid_name)) +
-      ggplot2::geom_sf(size = 0.05) + #, colour = "#dedfea"
+      ggplot2::geom_sf(size = 0.05, colour = "black") + #, colour = "#dedfea"
       ggplot2::scale_fill_distiller(palette = palette, direction = 1, na.value = "grey80") +
       ggplot2::labs(title = value) +
       ggplot2::theme_void() +
@@ -72,9 +73,47 @@ owid_map <- function(data, col = 3, palette = "Reds", mode = "plot") {
                      plot.title = ggplot2::element_text(hjust = 0.1, vjust = 0.3, face = "bold"),
                      plot.title.position = "plot")
   } else if (mode == "view") {
-    tmap::tmap_mode("view")
+    pal <- leaflet::colorNumeric(
+      palette = palette,
+      domain = map_data$value
+    )
+    pal_leg <- leaflet::colorNumeric(
+      palette = palette,
+      domain = map_data$value,
+      na.color = NA
+    )
+
+    labels <- sprintf(
+      "<strong>%s</strong><br/>Value: %g",
+      map_data$owid_name, map_data$value
+    ) %>% lapply(htmltools::HTML)
+
     map_data %>%
-      tmap::qtm("value")
+      leaflet::leaflet() %>%
+      leaflet:: addPolygons(
+        fillColor = ~pal(value),
+        weight = 0.2,
+        opacity = 1,
+        color = "black",
+        dashArray = "1",
+        fillOpacity = 0.7,
+        highlight = leaflet::highlightOptions(
+          weight = 2,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE
+        ),
+        label = labels,
+        labelOptions = leaflet::labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"
+        )
+      ) %>%
+      leaflet::addLegend(pal = pal_leg, values = ~value, opacity = 0.7, title = NULL,
+                         position = "bottomright", na.label = "") %>%
+      leaflet::addControl(paste0("<b>", value, "<b/>"), position = "topright")
   }
 
 
