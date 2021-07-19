6ba96136b67eeaceeec0e9f5c85c946872d4d0f1
piersyork <85755776+piersyork@users.noreply.github.com>
2021-07-19 21:28:37 +0100

    added owid_sources function

--- R/owid_url.R
+++ R/owid_url.R
@@ -1,6 +1,6 @@
 #' Get the GitHub and source URLs for a dataset
 #'
-#' @param data A tibble returned from `owid()`
+#' @param data A tibble returned from owid()
 #'
 #' @return Returns a named vector of the owid GitHub URL and the source URL.
 #' @export
@@ -19,4 +19,36 @@ owid_url <- function(data) {
       source_url, sep = "")
 
   invisible (c(github_url = github_url, source_url = source_url))
+}
+
+#' Show the source information of an owid dataset
+#'
+#' @param data A dataframe returned from owid() of class "owid".
+#'
+#' @return
+#' @export
+#'
+owid_sources <- function(data) {
+  if (!class(data)[1] == "owid") {
+    stop("Data must be of class owid")
+  }
+
+  sources <- attributes(data)$datapackage$sources
+
+  sou_vec <- lapply(sources, function(x) paste0(x, "\n\n")) %>%
+    unlist() %>%
+    na.omit()
+
+  sou_names <- sou_vec %>% names()
+  sou_names <- c("Name:", "Data Published By:", "Data Publisher Source",
+                 "Source Link:", "Date Retrieved:", "Additional Info:")
+
+  sources_out <- vector()
+  for (i in 1:length(sou_vec)) {
+    n <- i*2
+    sources_out[n] <- sou_vec[i]
+    sources_out[n-1] <- paste0(sou_names[i], "\n")
+  }
+
+  cat(sources_out, sep = "")
 }
