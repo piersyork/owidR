b84f883afeeb3dca5dcbbcc58a65cbea9bdedda6
piersyork <85755776+piersyork@users.noreply.github.com>
2021-07-19 18:43:43 +0100

    owid_url to include source url

--- R/owid_url.R
+++ R/owid_url.R
@@ -2,7 +2,7 @@
 #'
 #' @param data A tibble returned from `owid()`
 #'
-#' @return The GitHub URL of the dataset.
+#' @return Returns a named vector of the owid GitHub URL and the source URL.
 #' @export
 #'
 
@@ -10,6 +10,13 @@ owid_url <- function(data) {
   if (!class(data)[1] == "owid") {
     stop("Data must be of class owid")
   }
-  url <- attributes(data)$url
-  return (url)
+  github_url <- attributes(data)$url
+  source_url <- attributes(data)$datapackage$sources$link
+
+  cat("\nGitHub URL:\n",
+      github_url, "\n\n",
+      "Source URL:\n",
+      source_url, sep = "")
+
+  invisible (c(github_url = github_url, source_url = source_url))
 }
