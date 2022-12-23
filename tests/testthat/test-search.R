e77b58f8e16d18cc73a8d513f1852f037851dd5b
piersyork <piersyork@gmail.com>
2022-12-23 13:44:40 +0000

    add further search test

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -6,4 +6,7 @@ test_that("owid search gives object of class matrix and array", {
   search <- owid_search("gdp")
   expect_equal(class(search), c("matrix", "array"))
   expect_equal(class(search[1, 1]), "character")
+  expect_equal(length(search[, 2]), length(grep("gdp", search[, 2], ignore.case = TRUE)))
 })
+
+
