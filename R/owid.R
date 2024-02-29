de13eb46639be300d4d3c754a162a52efb190a30
Vegard Bøe <vgrdboee@gmail.com>
2024-02-29 13:50:56 +0100

    changed proposed solution to the out of bound error
    instead of adding an if else statement, I moved the placement of assigning a value on the source attribute. If the source attribute is missing from the metadata, and it's the first value that's added to the `data_info` list, the rest of the list can't be populated, and it returns the "out of bound" error.

--- R/owid.R
+++ R/owid.R
@@ -153,9 +153,9 @@ owid <- function(chart_id = NULL, rename = NULL, tidy.date = TRUE, ...) {
     colnames(out)[4] <- if (!is.null(display_name)) display_name else metadata$name
 
     data_info <- vector(mode = "list", length = 1)
-    data_info[[1]]$source <- if (is.null(metadata$source)) "" else metadata$source
     data_info[[1]]$dataset_name <- metadata$name
     data_info[[1]]$display <- metadata$display
+    data_info[[1]]$source <- metadata$source
   } else {
     tables <- grep(".*\\.data\\.json$", data_urls, value = TRUE) %>%
       lapply(\(x) jsonlite::fromJSON(x))
